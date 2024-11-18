4048761ea4830dd1623aa007169ab559fc52533f
gings <s@ging.ai>
2024-11-18 15:31:45 +0100

    update ex 04

--- ex04_OOD/run_resnet.py
+++ ex04_OOD/run_resnet.py
@@ -51,7 +51,7 @@ def main():
     parser.add_argument(
         "--data_dir",
         type=str,
-        default="/project/cv-ws2324/shared-data/data",
+        default="/project/cv-ws2425/lmb/data",
         help="where to load the data from",
     )
     args = parser.parse_args()
