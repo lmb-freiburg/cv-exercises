6de734e5024eda183f8d3a58524e4939f2cee423
arimousa <arian.mousakhan@gmail.com>
2025-10-20 14:34:59 +0200

    ex 01

---
+++ ex01/hello.py
@@ -0,0 +1,12 @@
+import sys
+
+import numpy as np
+
+
+def main():
+    print(f"Hello! Python version: {sys.version} ")
+    print(f"Numpy version: {np.version.version}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ ex01/lib/eigendecomp.py
@@ -0,0 +1,67 @@
+"""Eigendecomposition functions."""
+
+import numpy as np
+
+
+def get_matrix_from_eigdec(e: np.ndarray, V: np.ndarray) -> np.ndarray:
+    """Restore the original square symmetric matrix from eigenvalues and eigenvectors after eigenvalue decomposition.
+
+    Args:
+        e: The vector of eigenvalues with shape (N).
+        V: The matrix with eigenvectors as columns with shape (N, N).
+
+    Returns:
+        The original matrix used for eigenvalue decomposition with shape (N, N)
+    """
+    # START TODO #################
+    raise NotImplementedError
+    # END TODO ###################
+
+
+def get_euclidean_norm(v: np.ndarray) -> np.ndarray:
+    """Compute the euclidean norm of a vector.
+
+    Args:
+        v (np.ndarray): The input vector with shape (vector_length).
+
+    Returns:
+        The euclidean norm of the vector.
+    """
+    # START TODO #################
+    # Do not use np.linalg.norm
+    raise NotImplementedError
+    # END TODO ###################
+
+
+def get_dot_product(v1: np.ndarray, v2: np.ndarray) -> float:
+    """Compute dot product of two vectors.
+
+    Args:
+        v1: First input vector with shape (vector_length)
+        v2: Second input vector with shape (vector_length)
+
+    Returns:
+        Dot product result.
+    """
+    assert (
+        len(v1.shape) == len(v2.shape) == 1 and v1.shape == v2.shape
+    ), f"Input vectors must be 1-dimensional and have the same shape, but have shapes {v1.shape} and {v2.shape}"
+    # START TODO #################
+    raise NotImplementedError
+    # END TODO ###################
+
+
+def get_inverse(e: np.ndarray, V: np.ndarray) -> np.ndarray:
+    """Compute the inverse of a square symmetric matrix A given its eigenvectors and eigenvalues.
+
+    Args:
+        e: The vector of eigenvalues with shape (N).
+        V: The matrix with eigenvectors as columns with shape (N, N).
+
+    Returns:
+        The inverse of A (i.e. the matrix with given eigenvalues/vectors) with shape (N, N).
+    """
+    # START TODO #################
+    # Do not use np.linalg.inv
+    raise NotImplementedError
+    # END TODO ###################
